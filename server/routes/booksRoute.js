9e395396aaaee87aca0022b3f8cf8d5cdfbe8b20
Voldernn <nikitamm191103191103@gmail.com>
2024-05-04 21:18:56 +0300

    changed

--- server/routes/booksRoute.js
+++ server/routes/booksRoute.js
@@ -44,7 +44,7 @@ router.get('/', async (request, response) => {
   }
 });
 
-// Route for Get One Book from database by id
+
 router.get('/:id', async (request, response) => {
   try {
     const { id } = request.params;
