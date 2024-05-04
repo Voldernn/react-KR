2bdc9779d44cdce1db67935d99e925946aabc911
Voldernn <nikitamm191103191103@gmail.com>
2024-05-04 22:32:52 +0300

    index.js changed

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
+import { PORT, mongoDBURL } from '../config.js';
 import mongoose from 'mongoose';
-import booksRoute from './routes/booksRoute.js';
-import usersRoute from './routes/usersRoute.js';
+import booksRoute from '../routes/booksRoute.js';
+import usersRoute from '../routes/usersRoute.js';
 import cors from 'cors';
 
 const app = express();
 
-// Middleware for parsing request body
 app.use(express.json());
 app.use(cors());
 app.get('/', (request, response) => {
